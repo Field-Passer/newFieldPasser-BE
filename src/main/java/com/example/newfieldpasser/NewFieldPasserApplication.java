8c568a9593fcc228d964d7d05bd7d00e33f969fb
WONSEOKJE <nicholas4744@gmail.com>
2023-06-02 16:20:59 +0900

    conf: first commit

---
+++ src/main/java/com/example/newfieldpasser/NewFieldPasserApplication.java
@@ -0,0 +1,13 @@
+package com.example.newfieldpasser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFieldPasserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewFieldPasserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newfieldpasser/NewFieldPasserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newfieldpasser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewFieldPasserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
