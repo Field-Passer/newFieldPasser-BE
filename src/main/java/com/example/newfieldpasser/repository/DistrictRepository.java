f799c5c80a067ee85bd442cde7b0623916d3ef65
WONSEOKJE <nicholas4744@gmail.com>
2023-06-20 19:25:30 +0900

    feat: 지역 리스트 조회 구현 서울 지역으로 한정 DB에 구 저장

---
+++ src/main/java/com/example/newfieldpasser/repository/DistrictRepository.java
@@ -0,0 +1,8 @@
+package com.example.newfieldpasser.repository;
+
+import com.example.newfieldpasser.entity.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistrictRepository extends JpaRepository<District, Integer> {
+
+}
